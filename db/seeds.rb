19e232e0973d0840c068a84c6fe63e4f51f869b2
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-10-26 11:01:11 +0400

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+def create_seller
+  password = Faker::Internet.password(min_length: 8)
+  User.create(
+    username: Faker::Name.unique.name,
+    email: Faker::Internet.unique.email,
+    password: password,
+    password_confirmation: password,
+    role: 1
+  )
+end
+
+def create_buyer
+  password = Faker::Internet.password(min_length: 8)
+  User.create(
+    username: Faker::Name.unique.name,
+    email: Faker::Internet.unique.email,
+    password: password,
+    password_confirmation: password,
+    deposit: Faker::Number.within(range: 1..100) * 5
+  )
+end
+
+def create_product(seller_id)
+  Product.create(
+    product_name: Faker::Alphanumeric.alpha(number: Faker::Number.within(range: 1..20)),
+    amount_aviable: Faker::Number.within(range: 1..100),
+    cost: Faker::Number.within(range: 1..20) * 5,
+    seller_id: seller_id
+  )
+end
+
+50.times do
+  seller = create_seller
+  10.times do
+    create_product(seller.id)
+  end
+  create_buyer
+end
